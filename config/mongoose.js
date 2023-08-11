82a0bad97f75df5f790e3bfabc27fe236832b3f1
vishal9151 <84173784+vishal9151@users.noreply.github.com>
2023-08-12 01:18:14 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 // const env=require('./environment');
-mongoose.connect(`mongodb://127.0.0.1/Golf`);
+mongoose.connect("mongodb+srv://manglvisha:nCPs1KMFToGWXLDj@golfclub.ehe7jiy.mongodb.net/GolfClub?retryWrites=true&w=majority");
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error connecting to mongodb'));
 db.once('open',function(){
